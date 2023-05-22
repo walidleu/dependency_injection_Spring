57aff73d051650036b80c546aedc9c992a9b3913
walid leuleu <leuleu.walid.wl@gmail.com>
2023-05-22 21:59:54 +0100

    Initial commit

---
+++ src/main/java/metier/MetierImpl.java
@@ -0,0 +1,25 @@
+package metier;
+
+import dao.IDao;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MetierImpl implements IMetier {
+
+    private IDao dao;
+
+    public MetierImpl(IDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public double calcul() {
+        double tmp = dao.getData();
+        double res = tmp * 5 + Math.cos(tmp * Math.PI) / 100;
+        return res;
+    }
+
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+}
